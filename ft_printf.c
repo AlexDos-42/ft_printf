8a62c539439ab8630c36428eb2f9e8e8f4ce8220
Alex 42 <54394628+AlexDos-42@users.noreply.github.com>
2019-11-25 18:15:22 +0100

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -17,11 +17,16 @@ void   ft_flags(char **arg, t_parsing *parsing)
 
 void	ft_width(char **arg, t_parsing *parsing)
 {
-	
-	if (ft_isdigit(*arg))
+
+	if (*arg && ft_isdigit(*arg))
+	{
 		parsing->width = ft_atoi(*arg);
 		//incrémenter arg en f° atoi
-	else if (**arg == "*")		
+	}
+	else if (*arg && *arg == "*")	
+	{
+		*arg++;
+	}	
 	else
 		parsing->width = '\0';	
 
@@ -29,53 +34,137 @@ void	ft_width(char **arg, t_parsing *parsing)
 
 void	ft_precision(char **arg, t_parsing *parsing)
 {
-	if (*arg == '.')
+	if (*arg && *arg == '.')
 	{
 		arg++;
-
-
+		if (*arg && ft_isdigit(*arg))
+		{
+			parsing->precision = ft_atoi(*arg);
+			//incrémenter arg en f° atoi
+			else if (*arg && *arg == "*")	
+			{
+				*arg;
+			}
+			else	
+				parsing->precision = '\0';	
+		}
 	}
 }
 
-
 void	ft_lenght(char **arg, t_parsing *parsing)
 {
-	
+	if (*arg && *arg == 'l')
+	{
+		parsing->lenght = 'l';
+		if (*++arg == 'l')
+		{
+			parsing->lenght = 'll';
+			arg++;
+		}
+
+	}
+	else if (*arg && *arg == 'h')
+	{
+		parsing->lenght = 'h';
+		if (*++arg == 'h')
+		{
+			parsing->lenght = 'hh';
+			arg++;
+		}
+		else if (*arg == 'j' || *arg == 't' || *arg == 'z')
+			parsing->lenght = *arg++;
+		else
+			parsing->lenght = '\0';
+	}
 }
 
-int		ft_parsing(char *arg, va_list va)
+void ft_type(char **arg, t_parsing *parsing)
+{
+	if (*arg && *arg == c)
+		ft_typec
+	else if (*arg && *arg == s)
+		ft_types
+	else if (*arg && *arg == p)
+		ft_typep
+	else if (*arg && *arg == d)
+		ft_typed
+	else if (*arg && *arg == i)
+		ft_typei
+	else if (*arg && *arg == u)
+		ft_typeu
+	else if (*arg && (*arg == x || *arg == X))
+		ft_typexX
+	else if (*arg && *arg == f)
+		ft_typef
+	else if (*arg && *arg == g)
+		ft_typeg
+	else if (*arg && *arg == e)
+		ft_typee
+	else if (*arg && *arg == n)
+		ft_typen
+}
+
+void		ft_init_parsing(t_parsing *parsing)
+{
+	parsing->format = NULL;
+	parsing->width = NULL;
+	parsing->precision = NULL;
+	parsing->lenght = NULL;
+	parsing->type = NULL;
+	parsing->aff = NULL;
+}
+
+void		ft_parsing(char *arg, va_list va)
 {
 	int i;
-	t_parsing parsing;
 
-	ft_format(&arg, &parsin);
+	ft_format(&arg, &parsing);
 	ft_width(&arg, &parsing);
 	ft_precision(&arg, &parsing);
 	ft_lenght(&arg, &parsing);
-
-	return(i);
+	parsing->aff = ft_type(&arg, &parsing);
 }
-int		ft_printf(const char *format, ...)
+
+char 	*ft_format(char **arg, va_list va, t_parsing *parsing)
+{
+	char *tmp;
+
+	while (*arg)
+	{
+		ft_init_parsing(parsing);
+		if (*arg == '%')
+		{
+			arg++ += ft_parsing(&arg, va);
+			if (parsing->aff)
+				tmp = ft_strjoin(new_s, parsing->aff);
+		}
+		else 
+		//	tmp = 
+	free(parsing->aff);
+	free(put);
+	put = tmp;
+	}
+	return (put);
+}
+
+int	ft_printf(const char *format, ...)
 {
 	char *arg;
 	va_list	va;	
 	int len;
-	
+	char* put;
+	t_parsing *parsing;
+
+	if (format == NULL)
+		return (-1);
+	if (!(parsing = (t_parsing*)malloc(sizeof(t_parsingi))))
+		return (0);
 	va_start(va, format);
 	arg = (char *)format;
-	printf("test\n");
-	while (*arg)
-	{
-		if (*arg == '%')
-			arg++;
-			ft_parsing(arg, va);
-		else 
-			write(1, arg, 1);
-		++arg;
-	}
-	printf("testf\n");
+	put = ft_format(&arg, va, parsing);
+	ft_putstr_fd(put, 1);
+	len = ft_strlen(put);
 	va_end(va);
-	len = 1;
 	return (len);
 }
 
