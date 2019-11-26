52b3903db0c6228edc1758139d89363ec53afe83
Alex 42 <54394628+AlexDos-42@users.noreply.github.com>
2019-11-26 12:20:52 +0100

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -12,5 +12,6 @@ typedef struct		s_parsing
 
 void   ft_flags(char **arg, t_parsing *parsing);
 void   ft_width(char **arg, t_parsing *parsing);
+void	ft_precision(char **arg, t_parsing *parsing);
 
 #endef
