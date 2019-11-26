8c03feb0c8ba1bd9dc05e6f85774f26ef9100c27
Alex 42 <54394628+AlexDos-42@users.noreply.github.com>
2019-11-26 07:11:09 +0100

    Update ft_utile.c

--- ft_utile.c
+++ ft_utile.c
@@ -1,22 +1,6 @@
 # include "ft_printf.h"
 
-char	*ft_strjoin_char(char const *s1, char c)
-{
-	char	*new;
-	int		i;
-
-	i = 0;
-	if (!(new = malloc((ft_strlen(s1) + (c != 0) + 1) * sizeof(char))))
-		return (NULL);
-	while (s1 && *s1)
-		new[i++] = *s1++;
-	if (c)
-		new[i++] = c;
-	new[i] = '\0';
-	return (new);
-}
-
-char	*ft_strjoin(char const *s1, char c)
+char	*ft_strjoin_c(char const *s1, char c)
 {
 	int		i;
 	int		j;
@@ -33,11 +17,7 @@ char	*ft_strjoin(char const *s1, char c)
 		str[i] = s1[i];
 		i++;
 	}
-	while (s2[j] != '\0')
-	{
-		str[i + j] = s2[j];
-		j++;
-	}
-	str[i + j] = '\0';
+	str[i++] = c;
+	str[i] = '\0';
 	return (str);
 }
