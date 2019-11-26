f2ed19a9f9d41c34e4da57fb8138bd26cd0a4477
Alex 42 <54394628+AlexDos-42@users.noreply.github.com>
2019-11-26 12:21:37 +0100

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -1,5 +1,5 @@
 #ifndef FT_PRINTF_H
-#define FT_PRINTF_H
+# define FT_PRINTF_H
 
 typedef struct		s_parsing
 {
@@ -14,4 +14,4 @@ void   ft_flags(char **arg, t_parsing *parsing);
 void   ft_width(char **arg, t_parsing *parsing);
 void	ft_precision(char **arg, t_parsing *parsing);
 
-#endef
+#endif
