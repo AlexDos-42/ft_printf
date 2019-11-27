b1cfb14615634aff42f4ed9300936d0e48a6aa62
Alex 42 <54394628+AlexDos-42@users.noreply.github.com>
2019-11-28 00:21:37 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -1,64 +1,77 @@
 #include <stdio.h>
 #include "ft_printf.h"
 
-void ft_type(char **arg, va_list va, t_parsing *parsing)
+void ft_type(char type, va_list va, t_parsing *parsing)
 {
-	if (*arg == 'c')
+	if (type == 'c')
 		parsing->aff = ft_cdup(va_arg(*va, int));
-	else if (*arg == 's')
-		parsing->aff = ft_strdup(va_arg(*va, char*));
-	else if (*arg == 'd' || *arg == 'i')
-		parsing->aff = ft_itoa(va_arg(*va, int));
-	else if (*arg == 'u')
-		parsing->aff = ft_itoa_base(va_arg(*va, unsigned long), 10); 
-	else if (*arg == 'x')
-		parsing->aff = ft_itoa_base(va_arg(*ap, unsigned long), 16);
-	else if (*arg == 'X')
-		parsing->aff = ft_itoa_base(va_arg(*ap, unsigned long), 16);
-	else if (*arg == '%')
+	else if (type == '%')
 		parsing->aff = ft_cdup('%');
-	else if (*arg == 'p')
+	else if (type == 's')
+		parsing->aff = ft_strdup(va_arg(*va, char*));
+	else if (type == 'd' || *arg == 'i')
+		parsing->aff = ft_itoa(va_arg(*va, int));
+	else if (type == 'u')
+		parsing->aff = ft_itoa_base(va_arg(*va, unsigned long), 10); 
+	else if (type == 'x')
 		parsing->aff = ft_itoa_base(va_arg(*ap, unsigned long), 16);
+	else if (type == 'X')
+		parsing->aff = ft_itoa_base(va_arg(*ap, unsigned long), 16);
+	else if (type == 'p')
+		parsing->aff = ft_itoa_base(va_arg(*ap, unsigned long), 16);
+	returni
 }
 
 void		ft_init_parsing(t_parsing *parsing)
 {
-	parsing->flags- = 0;
+	parsing->flagstiret = 0;
 	parsing->flags0 = 0;
 	parsing->width = 0;
 	parsing->precision = 0;
-	parsing->lenght = 0;
+	//parsing->lenght = 0;
 	parsing->aff = NULL;
 }
 
 void		ft_parsing(char *arg, va_list va, parsing)
 {
-	ft_format(&arg, &parsing);
-	ft_width(&arg, va, &parsing);
-	ft_precision(&arg, va, &parsing);
-	ft_lenght(&arg, &parsing);
-	ft_type(&arg, va, &parsing);
+	int i;
+	
+	i = 0;
+	i +=ft_flags(&arg[i], parsing);
+	i +=ft_width(&arg[i], va, parsing);
+	i +=ft_precision(&arg[i], va, parsing);
+	//i +=ft_lenght(&arg[i], parsing);
+	ft_type(&arg[i], va, parsing);
+	if (&arg[i] == 'c' || &arg[i] == 'd' || &arg[i] == 'i' || &arg[i] == '%' || &arg[i] == 'x' ||
+		&arg[i] == 'X' ||  &arg[i] == 'p' ||  &arg[i] == 's')
+		return (i);
+	return (0);
 }
 
-char 	*ft_format(char **arg, va_list va, t_parsing *parsing)
+char 	*ft_boucle(char *arg, va_list va, t_parsing *parsing)
 {
 	char *tmp;
 	char *put;
 	
 	while (*arg)
 	{
-		ft_init_parsing(parsing);
+		
 		if (*arg == '%')
 		{
-			arg++ += ft_parsing(&arg, va, parsing);
+			ft_init_parsing(parsing);
+			arg++;
+			arg += ft_parsing(arg, va, parsing);
 			if (parsing->aff)
 				tmp = ft_strjoin(put, parsing->aff);
 		}
 		else 
+		{
 			tmp = ft_strjoin_c(put, parsing->aff);
-	free(parsing->aff);
-	free(put);
-	put = tmp;
+			arg++;
+		}
+		free(parsing->aff);
+		free(put);
+		put = tmp;
 	}
 	return (put);
 }
@@ -77,7 +90,7 @@ int	ft_printf(const char *format, ...)
 		return (0);
 	va_start(va, format);
 	arg = (char *)format;
-	put = ft_format(&arg, va, parsing);
+	put = ft_boucle(&arg, va, parsing);
 	ft_putstr_fd(put, 1);
 	len = ft_strlen(put);
 	va_end(va);
