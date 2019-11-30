0aad5de6d6a99a10f1c0a21280efc718564db57d
Alex 42 <54394628+AlexDos-42@users.noreply.github.com>
2019-11-30 23:47:43 +0100

    Update ft_utile.c

--- ft_utile.c
+++ ft_utile.c
@@ -37,27 +37,25 @@ char	*ft_strjoin_c(char const *s1, char c)
 	return (str);
 }
 
-char	*ft_itoa_base(int nbr, int base)
+char	*ft_itoa_base(int nbr, char *base)
 {
 	char	*str;
-	int		i;
-	int		sign;
-	int		n;
-
+	int	i;
+	int	sign;
+	int	n;
+	int 	basesize;
+	
 	i = 0;
-	sign = (nbr < 0 && base == 10) ? 1 : 0;
-	n = (nbr < 0) ? -nbr : nbr;
+	base_size = ft_strlen(base);
 	while ((n /= base) >= 1)
 		i++;
 	i += 1;
-	n = (nbr < 0) ? -nbr : nbr;
 	if (!(str = (char *)ft_calloc(sizeof(char), (i + 1))))
 		return (NULL);
 	while (i--)
 	{
-		str[i + sign] = (n % base < 10) ? n % base + '0' : n % base + 'a' - 10;
+		str[i + sign] = base[(n % base < 10) ? n % base + '0' : n % base + 'a' - 10];
 		n /= base;
 	}
-	(sign) ? str[0] = '-' : 0;
 	return (str);
 }
