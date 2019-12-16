11eb5518ccf2c48b91a7433dd7be179e6533505c
Alex 42 <54394628+AlexDos-42@users.noreply.github.com>
2019-12-16 14:37:37 +0100

    Add files via upload

---
+++ srcs/ft_parsing.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_parsing.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alesanto <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/11 13:00:03 by alesanto          #+#    #+#             */
+/*   Updated: 2019/12/16 13:25:38 by alesanto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/ft_printf.h"
+
+int			ft_flags(char *arg, t_parsing *parsing)
+{
+	int	i;
+
+	i = 0;
+	if (*arg)
+	{
+		while (arg[i] == '-' || arg[i] == '+'
+				|| arg[i] == '0' || arg[i] == ' ' || arg[i] == '#')
+		{
+			if (arg[i] == '-')
+				parsing->flagstiret = -1;
+			if (arg[i] == '0' && parsing->flagstiret != -1)
+			{
+				parsing->flags0 = 1;
+				parsing->z = 1;
+			}
+			if (parsing->flagstiret == -1)
+			{
+				parsing->flags0 = 0;
+				parsing->z = 0;
+			}
+			i++;
+		}
+	}
+	return (i);
+}
+
+int			ft_width(char *arg, va_list *va, t_parsing *parsing)
+{
+	int i;
+	int j;
+
+	i = 0;
+	if (*arg && ft_isdigit(arg[i]))
+	{
+		parsing->flagstiret == 0 ?
+			parsing->flagstiret = 1 : parsing->flagstiret;
+		parsing->flagstiret *= ft_atoi(arg);
+		parsing->flags0 *= parsing->flagstiret;
+		while (ft_isdigit(arg[i]))
+			i++;
+	}
+	else if (*arg && arg[i] == '*')
+	{
+		parsing->flagstiret == 0 ?
+			parsing->flagstiret = 1 : parsing->flagstiret;
+		j = parsing->flagstiret > 0 ? 1 : -1;
+		parsing->flagstiret *= va_arg(*va, unsigned int);
+		if (j == -1 && parsing->flagstiret > 0)
+			parsing->flagstiret = -parsing->flagstiret;
+		parsing->flags0 *= parsing->flagstiret;
+		i++;
+	}
+	return (i);
+}
+
+int			ft_precision(char *arg, va_list *va, t_parsing *parsing)
+{
+	int i;
+
+	i = 0;
+	if (*arg && arg[i] == '.')
+	{
+		arg++;
+		parsing->precision = 0;
+		if (*arg && ft_isdigit(arg[i]))
+		{
+			parsing->precision = ft_atoi(arg);
+			while (ft_isdigit(arg[i]))
+				i++;
+		}
+		else if (*arg && arg[i] == '*')
+		{
+			parsing->precision = va_arg(*va, unsigned int);
+			i++;
+		}
+		i++;
+	}
+	return (i);
+}
---
+++ srcs/ft_utile.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utile.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alesanto <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/11 12:58:26 by alesanto          #+#    #+#             */
+/*   Updated: 2019/12/16 13:26:07 by alesanto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/ft_printf.h"
+
+char		*ft_cdup(int d)
+{
+	char	*str;
+	int		i;
+	char	c;
+
+	c = (char)d;
+	i = 0;
+	if (!c)
+	{
+		if (!(str = malloc(1 * sizeof(char))))
+			return (NULL);
+	}
+	else
+	{
+		if (!(str = malloc((2) * sizeof(char))))
+			return (NULL);
+	}
+	if (c)
+		str[i++] = c;
+	str[i] = '\0';
+	return (str);
+}
+
+char		*ft_strjoin_c(char const *s1, char c)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	if (!c)
+		return (NULL);
+	if (!(str = malloc(sizeof(char) * (ft_strlen(s1) + 2))))
+		return (NULL);
+	while (s1 && *s1)
+	{
+		str[i] = *s1++;
+		i++;
+	}
+	str[i++] = c;
+	str[i] = '\0';
+	return (str);
+}
+
+char		*ft_itoa_base(unsigned long long int nbr, char *base)
+{
+	char						*str;
+	int							i;
+	unsigned long long int		n;
+	int							basesize;
+
+	i = 0;
+	basesize = ft_strlen(base);
+	n = nbr;
+	while ((n /= basesize) >= 1)
+		i++;
+	i += 1;
+	if (!(str = (char *)ft_calloc(sizeof(char), (i + 1))))
+		return (NULL);
+	while (i--)
+	{
+		str[i] = base[nbr % basesize];
+		nbr /= basesize;
+	}
+	return (str);
+}
